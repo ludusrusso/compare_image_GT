eb07ab7608b0b7cacfc22cec4cf7321f1e55807d
Ludovico Russo <ludus.russo@gmail.com>
2014-09-05 17:05:11 +0200

    add graphics interface

--- compare.cpp
+++ compare.cpp
@@ -1,4 +1,5 @@
 /*
+        std::cout << compare.size() << " "<< odom.size() << std::endl;
     Copyright (C) 2014 Politecnico di Torino
 
 
@@ -37,11 +38,13 @@ class ImgsData{
   public:
     ImgsData(string sdata) {
       stringstream ss(sdata);
-      ss >> file >> ID >> x >> y;
-      std::cout << file << " "<< ID << " "<< x << " "<< y << std::endl;
+      /* ss >> file >> ID >> x >> y; */
+      ss >> file >> x >> y;
+      /* std::cout << file << " "<< ID << " "<< x << " "<< y << std::endl; */
+      std::cout << file << " "<< " "<< x << " "<< y << std::endl;
     }
     string file;
-    int ID;
+    /* int ID; */
     double x, y;
 };
 
@@ -65,33 +68,67 @@ int main(int argc, char *argv[])
 
   std::vector<ImgsData> imgs;
 
+  double max_x = -10000;
+  double max_y = -10000;
+  double min_x = 10000;
+  double min_y = 10000;
+
   while (!inf.eof()) {
     string line;
     getline(inf, line);
     if (line.size() == 0) break;
-    imgs.push_back(ImgsData(line));
+    ImgsData im = ImgsData(line);
+    imgs.push_back(im);
+
+    if (max_x < im.x) max_x = im.x;
+    if (max_y < im.y) max_y= im.y;
+    if (min_x > im.x) min_x = im.x;
+    if (min_y > im.y) min_y = im.y;
+  }
+
+  int tdist = 50;
+
+  Mat img = imread(imgs[0].file);
+  Size newsize(scale*img.size().width, scale*img.size().height);
+
+  Mat odom = Mat::zeros(newsize.height*2, newsize.height*2, CV_8UC3);
+
+  for (int i = 0; i < (int)imgs.size(); ++i)
+  {
+    Point p((imgs[i].x - min_x)*odom.size().height/(max_x-min_x), -(imgs[i].y - max_y)*odom.size().height/(max_y-min_y));
+    circle(odom, p, 1, Scalar(255,255,255));
   }
 
 
   for (int i = 0; i < (int)imgs.size(); ++i)
   {
-    for (int j = 0; j < i; ++j)
+    for (int j = 0; j < i - tdist; ++j)
     {
-      if (sqrt(pow(imgs[i].x - imgs[j].x,2) + pow(imgs[i].y - imgs[j].y,2)) < tresh ){
+      double dist = sqrt(pow(imgs[i].x - imgs[j].x,2) + pow(imgs[i].y - imgs[j].y,2));
+      if (dist < tresh ){
+        std::cout << imgs[i].file << " " << imgs[j].file << endl;
+        std::cout << "( " <<imgs[i].x<< " , "<< imgs[i].y << " ) -- "<< "( " <<imgs[j].x<< " , "<< imgs[j].y << " ) -- " << dist << std::endl;
         Mat img1, img2;
         img1 = imread(imgs[i].file);
         img2 = imread(imgs[j].file);
 
-        Size newsize(scale*img1.size().width, scale*img1.size().height);
+
+        Mat podom(odom.clone());
+
+        Point p1((imgs[i].x - min_x)*odom.size().height/(max_x-min_x), -(imgs[i].y - max_y)*odom.size().height/(max_y-min_y));
+        Point p2((imgs[j].x - min_x)*odom.size().height/(max_x-min_x), -(imgs[j].y - max_y)*odom.size().height/(max_y-min_y));
+        circle(podom, p1, 5, Scalar(0,0,255), -1);
+        circle(podom, p2, 5, Scalar(0,255,0), -1);
 
         resize(img1, img1, newsize);
         resize(img2, img2, newsize);
         Mat compare;
         vconcat(img1, img2, compare);
+        hconcat(compare,podom, compare);
         imshow("Type Y/N", compare);
         char res = waitKey(-1);
         if (res == 'y' || res == 'Y') {
-          of << imgs[i].ID << " " << imgs[j].ID << endl;
+          of << imgs[i].file << " " << imgs[j].file << endl;
         }
       }
     }
